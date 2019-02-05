210895187cc89b0dc86526d3a72b51907510f523
Tuomas Nummela <tuomasnummela@Tuomas-MacBook-Pro-2.local>
2019-02-05 13:11:13 +0200

    changes

--- src/index.jsx
+++ src/index.jsx
@@ -8,7 +8,7 @@ import './assets/stylesheets/style.css'
 /****** ADD YOUR CODE AFTER THIS LINE ******/
 
 const Hello = () => (
-  <div>Hello World! Waddup</div>
+  <div>Hello World! Waddup testing</div>
 );
 
 class App extends Component {
